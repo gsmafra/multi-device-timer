17b0de945b7ac5d725431456ee6d023180c6bd38
gsmafra <gsenamafra@gmail.com>
2025-03-20 02:42:19 -0300

    allow unsafe werkzeug

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ def on_connect():
 
 # Start the Flask app with SocketIO
 if __name__ == '__main__':
-    socketio.run(app, debug=False)
+    socketio.run(app, debug=False, allow_unsafe_werkzeug=True)
