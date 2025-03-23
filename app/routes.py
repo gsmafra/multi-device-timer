a53252a4090eeacccfa31b3ceb137ff8da43436d
gsmafra <gsenamafra@gmail.com>
2025-03-23 01:58:36 -0300

    use firebase url env variable

--- app/routes.py
+++ app/routes.py
@@ -8,24 +8,25 @@ from flask_socketio import emit
 import firebase_admin
 from firebase_admin import credentials, db
 
-# Create a blueprint for our routes
 bp = Blueprint('routes', __name__)
 
-# Initialize Firebase Admin SDK if not already initialized.
-if not firebase_admin._apps:
+def init_firebase():
     cred = credentials.Certificate(environ['FIREBASE_KEY_PATH'])
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://multi-device-timer-default-rtdb.firebaseio.com/'
-    })
+    firebase_admin.initialize_app(cred, {'databaseURL': environ['FIREBASE_URL']})
 
-# Get a reference to the Firebase realtime database
-timer_ref = db.reference('timer')
+# Call the initialization function during blueprint registration or app startup.
+init_firebase()
 
-# Import the shared SocketIO instance from __init__.py
+def get_timer_ref():
+    return db.reference('timer')
+
+def get_active_ref():
+    return db.reference('active_device')
+
 from app import socketio
 
-# Timer function using a fixed end_time for accuracy
 def run_timer(end_time):
+    timer_ref = get_timer_ref()
     while True:
         time.sleep(0.5)  # Check more frequently for smoother updates
         current_time = time.time()
@@ -46,6 +47,7 @@ def run_timer(end_time):
 
 @bp.route('/start/<int:duration>')
 def start_timer(duration):
+    timer_ref = get_timer_ref()
     # Stop any currently running timer by updating the DB
     timer_ref.update({'running': False})
 
@@ -60,12 +62,12 @@ def start_timer(duration):
 
 @bp.route('/pause')
 def pause_timer():
-    timer_ref.update({'running': False})
+    get_timer_ref().update({'running': False})
     return 'Timer paused'
 
 @bp.route('/claim_active/<deviceid>')
 def claim_active(deviceid):
-    active_ref = db.reference('active_device')
+    active_ref = get_active_ref()
     active_ref.set(deviceid)
     socketio.emit('active_device', {'active_device': deviceid})
     return 'Active device claimed'
@@ -80,9 +82,11 @@ def index():
 
 @socketio.on('connect')
 def on_connect():
+    timer_ref = get_timer_ref()
     timer_data = timer_ref.get()
     time_left = timer_data['time_left'] if timer_data else 0
     emit('update_timer', {'time_left': time_left})
-    active_ref = db.reference('active_device')
+    
+    active_ref = get_active_ref()
     active = active_ref.get() or ''
     emit('active_device', {'active_device': active})
